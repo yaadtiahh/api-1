cca99c181bbcbb1af60333def192387b7e563b21
Lleanin <Fifashf@yandex.ru>
2025-10-21 18:02:18 +0200

    .

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+import requests
+from dotenv import load_dotenv
+from datetime import datetime
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+
+def main():
+    url = "https://calendarific.com/api/v2/holidays"
+    payload = {
+      "api_key": API_KEY,
+      "country": "RU",
+      "year": "2025"
+    }
+
+    response = requests.get(url, params=payload)
+    holidays = response.json()["response"]["holidays"]
+
+    months_ru = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"]
+
+    for holiday in holidays:
+        day = holiday["date"]["datetime"]["day"]
+        month = holiday["date"]["datetime"]["month"]
+        month = months_ru[month-1]
+
+        name = holiday["name"]
+        description = holiday["description"]
+
+        print(f"Дата: {day} {month}, ")
+        print(f"Назавание праздника: {name}, ")
+        print(f"Описание: {description} \n")
+
+
+if __name__ == '__main__':
+    main()
